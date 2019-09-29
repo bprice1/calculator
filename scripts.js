71a387738b5ee6232fdec248ab07c78723883c46
bprice1 <brian.price@student.swic.edu>
2019-09-29 00:44:01 -0500

    Add selectors to pull data from form and show on console.log.

--- scripts.js
+++ scripts.js
@@ -29,9 +29,12 @@ function remainder(a, b) {
  */
 
 const form = document.querySelector("form");
+const num1 = document.querySelector("#num-1");
+const num2 = document.querySelector("#num-2");
+const select = document.querySelector("select");
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  console.log("Submitted");
+  console.log(num1.value, num2.value, select.value);
 });
