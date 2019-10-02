baf31af29d73dc0d911f95382a95d8a889cbc50a
brian.price <brian.price@student.swic.edu>
2019-10-02 18:36:07 -0500

    Add function used.

--- scripts.js
+++ scripts.js
@@ -32,9 +32,19 @@ const form = document.querySelector("form");
 const num1 = document.querySelector("#num-1");
 const num2 = document.querySelector("#num-2");
 const select = document.querySelector("select");
+const output = document.querySelector("output");
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
 
+  /**
+   * Get the values from the input fields.
+   * Pass teh values in as arguments to the function.
+   *  * TODO: Figure out how to dynamically call a function by name.
+   * Display results.
+   */
   console.log(num1.value, num2.value, select.value);
+  console.log(add(num1.value, num2.value));
+
+  output.textContent = add(num1.value, num2.value);
 });
